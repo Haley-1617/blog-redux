922fddfe101c758b5d9ab9edfb1fbe4d2de6484c
Haley-1617 <alicelai2920@gmail.com>
2021-07-01 12:40:42 +0800

    Refactored code

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,16 @@ import jsonPlaceholder from "../api/jsonPlaceholder";
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, "userId"));
-  userIds.forEach((id) => dispatch(fetchUser(id)));
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  // getState().posts is the pass value of .map() which is the pass value of .uniq() and so on
+  // add .value() in the end to execute _.chain()
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
 
 // function which returns a function
